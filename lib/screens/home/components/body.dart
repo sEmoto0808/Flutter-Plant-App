03fd19216e4ae2c9b52987bb05e44c7403b999f6
Sho Emoto <dd2d00seg1@gmail.com>
2021-11-25 21:27:34 +0900

    refactoring

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_plant_app/constants.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+import 'header_with_search_box.dart';
 
 class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -11,99 +13,6 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    Key? key,
-    required this.size,
-  }) : super(key: key);
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: size.height * 0.2,
-      child: Stack(
-        children: [
-          Container(
-            padding:
-                const EdgeInsets.only(
-                  left: kDefaultPadding,
-                  right: kDefaultPadding,
-                  bottom: 36 + kDefaultPadding,
-                ),
-            height: size.height * 0.2 - 27,
-            decoration: const BoxDecoration(
-              color: kPrimaryColor,
-              // Only bottom curve
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  'Hi Uishopy!',
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                const Spacer(),
-                Image.asset('assets/images/logo.png'),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              margin:
-                  const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding:
-                  const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        // suffix isn't working property with SVG
-                        // that why use Row
-                        // suffixIcon: SvgPicture.asset('assets/icons/search.svg')
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset('assets/icons/search.svg'),
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
